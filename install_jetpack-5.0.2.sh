8a9dc5b56437fff7700f2cfe0e7fe0e16830b070
Kevin Eales <manbehindthemadness@gmail.com>
2023-07-27 13:07:41 -0400

    Update install_jetpack-5.0.2.sh

--- install_jetpack-5.0.2.sh
+++ install_jetpack-5.0.2.sh
@@ -13,22 +13,23 @@
 ######## ENSURE YOU HAVE SETUP A USERNAME AND PASSWORD ON THE ORIN #########
 ################ YOU WILL BE PROMPTED FOR IT DURING SETUP ##################
 ############################################################################
-############### DOWNLOAD DOCKER IMAGE HERE: USE 20.04 ######################
+############### DOWNLOAD DOCKER IMAGE HERE: USE 18.04 ######################
 ############ https://developer.nvidia.com/drive/sdk-manager ################
 ############################################################################
 ############## USE THESE TO LOAD THE DOWNLOADED IMAGE ######################
-## NOTE: 20.04 sdkmanager 1.9.2.10899 docker image is broken and will fail #
-## sudo docker load -i ./sdkmanager-1.9.2.10899-Ubuntu_18.04_docker.tar.gz #
-## sudo docker tag sdkmanager-1.9.2.10899-Ubuntu_18.04_docker.tar.gz #######
+## NOTE: 20.04 sdkmanager 1.9.3.10904 docker image is broken and will fail #
+## sudo docker load -i ./sdkmanager-1.9.3.10904-Ubuntu_18.04_docker.tar.gz #
+## sudo docker tag sdkmanager-1.9.3.10904-Ubuntu_18.04_docker.tar.gz #######
+## NOTE: The sdkmanager must be at the latest version as updating fails ####
 ############################################################################
 
 read -p "Welcome to the single most terrible, mind-crushing, nightmarish, and horrible flashing utility ever designed. Lets get started..."
 
 echo listing possible commands:
-sudo docker run -it --privileged -v /dev/bus/usb:/dev/bus/usb/ -v /dev:/dev -v /media/$USER:/media/nvidia:slave --rm sdkmanager:1.9.2.10899-Ubuntu_18.04 --query
+sudo docker run -it --privileged -v /dev/bus/usb:/dev/bus/usb/ -v /dev:/dev -v /media/$USER:/media/nvidia:slave --rm sdkmanager:1.9.3.10904-Ubuntu_18.04 --query
 
 echo detecting connected devices...
-sudo docker run -it --privileged -v /dev/bus/usb:/dev/bus/usb/ -v /dev:/dev -v /media/$USER:/media/nvidia:slave --rm sdkmanager:1.9.2.10899-Ubuntu_18.04 --listconnected all
+sudo docker run -it --privileged -v /dev/bus/usb:/dev/bus/usb/ -v /dev:/dev -v /media/$USER:/media/nvidia:slave --rm sdkmanager:1.9.3.10904-Ubuntu_18.04 --listconnected all
 
 read -p "If no devices are connected abort the script, ensure the host is connected via the usb-c port next to the 40 pin header, then restart the unit while pressing the recovery (middle) button"
 
@@ -45,7 +46,7 @@ read -p "Optional: skip the install of the nvidia sdk components, perform an apt
 ############################################################################
 ########################## INSTALL FULL SYSTEM #############################
 ############################################################################
-sudo docker run -it --privileged -v /dev/bus/usb:/dev/bus/usb/ -v /dev:/dev -v /media/$USER:/media/nvidia:slave sdkmanager:1.9.2.10899-Ubuntu_18.04 --cli install --logintype devzone --product Jetson --version 5.1.1 --targetos Linux --host --target JETSON_AGX_ORIN_TARGETS --flash all --license accept --checkforupdates force --additionalsdk 'DeepStream 6.2'
+sudo docker run -it --privileged -v /dev/bus/usb:/dev/bus/usb/ -v /dev:/dev -v /media/$USER:/media/nvidia:slave sdkmanager:1.9.3.10904-Ubuntu_18.04 --cli install --logintype devzone --product Jetson --version 5.1.1 --targetos Linux --host --target JETSON_AGX_ORIN_TARGETS --flash all --license accept --checkforupdates force --additionalsdk 'DeepStream 6.2'
 
 echo At this point you have probably experienced a weird failure: FEAR NOT! The nvidia sdkmanager is completely unstable, so just re-run this script! There is an 80% probability you will achieve different results!
 
